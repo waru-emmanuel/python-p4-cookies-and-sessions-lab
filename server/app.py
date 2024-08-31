5d898d6d80af1329851f9d6f206efb9c2b3cfa9e
EMMANUEL <waruterewachira7@gmail.com>
2024-08-31 07:05:32 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, session
+from flask import Flask, jsonify, session
 from flask_migrate import Migrate
-
-from models import db, Article, User
+from models import db, Article
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
@@ -12,7 +11,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
 @app.route('/clear')
@@ -22,13 +20,24 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    articles = Article.query.all()
+    return jsonify([article.to_dict() for article in articles]), 200
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    # Initialize page_views in the session if not already set
+    session['page_views'] = session.get('page_views', 0)
 
-    pass
+    # Increment the page_views count
+    session['page_views'] += 1
+
+    # Check if the user has exceeded the maximum number of allowed page views
+    if session['page_views'] > 3:
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
+
+    # Return the article data if within the allowed page views limit
+    article = Article.query.get_or_404(id)
+    return jsonify(article.to_dict()), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
